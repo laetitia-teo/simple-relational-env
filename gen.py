7bd727de2480ca48dfb957c63d332c5853dff8f9
Laetitia Teodorescu <laetitia.teodorescu@gmail.com>
2019-10-03 16:27:55 +0200

    updated data saving and loading

--- gen.py
+++ gen.py
@@ -14,6 +14,7 @@ import torch
 from dataset import Dataset
 
 from env import SamplingTimeout
+from utils import to_file, from_file
 
 class AbstractGen():
     """
--- utils.py
+++ utils.py
@@ -13,9 +13,12 @@ def to_file(data, path):
     TODO : make this more optimized and more general, for several data types.
     """
     with open(path, 'a') as f:
-        for vec in data:
-            for num in vec:
-                f.write(str(num) + ' ')
+        f.write('\n')
+        for vec_list in data:
+            for vec in vec_list:
+                for num in vec:
+                    f.write(str(num) + ' ')
+                f.write('\n')
             f.write('\n')
 
 def from_file(path, dtype=float):
@@ -23,11 +26,18 @@ def from_file(path, dtype=float):
     Reads a list of vectors from a file at path.
     """
     with open(path, 'r') as f:
+        data = []
         vec_list = []
+        state = 0 # new array
         for l in f.readlines():
             str_list = l.split(' ')
-            num_list = []
-            for num in str_list[:-1]:
-                num_list.append(dtype(num))
-            vec_list.append(np.array(num_list))
-    return vec_list
+            if str_list == ['\n']:
+                if vec_list:
+                    data.append(vec_list)
+                vec_list = []
+            else:
+                num_list = []
+                for num in str_list[:-1]:
+                    num_list.append(dtype(num))
+                vec_list.append(np.array(num_list))
+    return data
