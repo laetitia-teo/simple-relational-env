f41b7fadad54d7db58f88a4a012e64726ea8ef7e
Laetitia Teodorescu <laetitia.teodorescu@gmail.com>
2019-10-03 15:59:37 +0200

    small data loading and saving utilities

--- utils.py
+++ utils.py
@@ -1,10 +1,33 @@
 """
 Small utilities.
 """
-
+import os.path as op
 import numpy as np
 
-def to_file(data):
+from glob import glob
+
+def to_file(data, path):
     """
     Writes a list of vectors to a file at path.
+
+    TODO : make this more optimized and more general, for several data types.
     """
+    with open(path, 'a') as f:
+        for vec in data:
+            for num in vec:
+                f.write(str(num) + ' ')
+            f.write('\n')
+
+def from_file(path, dtype=float):
+    """
+    Reads a list of vectors from a file at path.
+    """
+    with open(path, 'r') as f:
+        vec_list = []
+        for l in f.readlines():
+            str_list = l.split(' ')
+            num_list = []
+            for num in str_list[:-1]:
+                num_list.append(dtype(num))
+            vec_list.append(np.array(num_list))
+    return vec_list
