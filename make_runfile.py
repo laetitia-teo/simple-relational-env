7fad39f1aeedbfd717b8eaa283765dc53010a154
laetitia-teo <laetitia.teodorescu@gmail.com>
2020-04-16 18:52:26 +0200

    minor changes

--- make_runfile.py
+++ make_runfile.py
@@ -6,7 +6,6 @@ configlist = sorted([re.search(r'config([0-9]+)', p)[1] for p in paths])
 
 s = """#!/bin/sh
 #SBATCH --mincpus 12
-#SBATCH -p routage
 #SBATCH -t 60:00:00
 #SBATCH -e experimental_results/expe{0}.err
 #SBATCH -o experimental_results/expe{0}.out
